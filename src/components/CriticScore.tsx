1996a824dedbb00090dc13be210043fad1fb3d73
kanavguptaa <64109848+kanavguptaa@users.noreply.github.com>
2024-08-10 19:19:47 -0500

    displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,14 @@
+import { Badge } from '@chakra-ui/react';
+import React from 'react'
+
+interface Props{
+    score:number;
+}
+const CriticScore = ({score}:Props) => {
+    let color = score > 75 ? "green" : score > 60 ? "yellow" : ""; 
+  return (
+    <Badge borderRadius={"5px"} fontSize="14px" paddingX={2} colorScheme={color}>{score}</Badge>
+  )
+}
+
+export default CriticScore
