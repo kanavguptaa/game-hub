6be6216bf4598aa11072485a023f9c493fb38cba
kanavguptaa <64109848+kanavguptaa@users.noreply.github.com>
2024-08-10 20:27:11 -0500

    show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
 
 export interface Platform {
     id:number;
@@ -22,15 +24,28 @@ interface FetchGameResponse {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const[isLoading, setLoading] = useState(false);
 
+    // setLoading(true);   
     useEffect (() =>{
+        const controller = new AbortController();
+        
+        setLoading(true)
         apiClient
         .get<FetchGameResponse>("/games")
-        .then((res) => setGames(res.data.results))
-        .catch((err) => setError(err.message) )
+        .then((res) => { 
+            setGames(res.data.results)
+            setLoading(false)
+        })
+        .catch((err) =>{ if (err instanceof CanceledError) return;
+            setError(err .message);
+            setLoading(false);
+        } )
+
+    return () => controller.abort()
     },[]);
 
-    return {games, error}
+    return {games, error, isLoading };
 }
 
 export default useGames;
